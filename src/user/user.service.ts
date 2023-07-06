2dfa1170561df5a9c74a55ee3fc0570cd6b4cde7
Eliot Fagnon <efagnon2002@gmail.com>
2023-07-06 11:06:22 +0100

    feat: add service for user creation and retrieval

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
+import { CreateUserDto } from "./dtos/create-user.dto";
+import { User } from "@prisma/client";
+import bcrypt from "bcrypt";
 
 @Injectable()
 export class UserService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async create(): {};
+  async create(payload: CreateUserDto): Promise<User> {
+    payload.password = await bcrypt.hash(payload.password, 10);
+
+    const user: User = await this.prismaService.user.create({
+      data: payload,
+    });
+
+    return user;
+  }
+
+  async findById(id: string): Promise<User> {
+    const user: User = await this.prismaService.user.findUniqueOrThrow({
+      where: { id }
+    });
+
+    return user;
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.prismaService.user.findMany();
+  }
 }
