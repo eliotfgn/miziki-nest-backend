acac8864bb12fddf6ee5af2b755acca33a5c58ae
Eliot Fagnon <efagnon2002@gmail.com>
2023-07-06 11:43:55 +0100

    feat: user registration logic

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUser } from "../dtos/create-user.dto";
+import { UserService } from "../user.service";
+import { User } from "@prisma/client";
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+  async register(payload: CreateUser): Promise<User> {
+    return await this.userService.create(payload);
+  }
+
+  async login(payload: LoginRequest) {
+
+  }
+}
